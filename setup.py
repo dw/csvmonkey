991798b53fa97ac6c2e297232c79c638fb187adf
David Wilson <dw@botanicus.net>
2017-05-05 09:35:50 +0000

    flags

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ extra_compile_args += ['-msse4.2']
 extra_compile_args += ['-std=c++11']
 extra_compile_args += ['-O3']
 extra_compile_args += ['-w']
-#extra_compile_args += ['-UNDEBUG']
+extra_compile_args += ['-DUSE_SPIRIT']
+extra_compile_args += ['-I/home/dmw/src/boost_1_64_0']
+#extra_compile_args += ['-fprofile-generate', '-lgcov']
+# extra_compile_args += ['-UNDEBUG']
+extra_compile_args += ['-DNDEBUG']
 
 setup(
     name = 'py-csvninja',
