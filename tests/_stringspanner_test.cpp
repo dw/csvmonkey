8782dac8bddf63acbf57ea8b40746e4b725cc9a3
David Wilson <dw@botanicus.net>
2019-05-05 01:49:55 +0100

    Add a bunch of incomplete tests from long ago.

--- tests/_stringspanner_test.cpp
+++ tests/_stringspanner_test.cpp
@@ -20,3 +20,54 @@ TEST_CASE(PREFIX "midNullTerminates", "[stringspanner]")
     csvmonkey::StringSpanner ss(',');
     REQUIRE(ss(x) == 16);
 }
+
+
+TEST_CASE(PREFIX "noMatchTerminates0", "[stringspanner]")
+{
+    // Comma not found.
+    const char *x = "derpderpderpderpderp";
+    csvmonkey::StringSpanner ss(',');
+    REQUIRE(ss(x) == 16);
+}
+
+
+TEST_CASE(PREFIX "noMatchTerminates1", "[stringspanner]")
+{
+    // No terminator specified.
+    const char *x = "derpderpderpderpderp";
+    csvmonkey::StringSpanner ss;
+    REQUIRE(ss(x) == 16);
+}
+
+
+TEST_CASE(PREFIX "matchAtEachOffset", "[stringspanner]")
+{
+    const char *x = "derpderpderpderpderp";
+    for(int i = 0; i < 16; i++) {
+        std::string s(x);
+        s[i] = ',';
+        INFO("i = " << i);
+        csvmonkey::StringSpanner ss(',');
+        REQUIRE(ss(s.c_str()) == i);
+    }
+}
+
+
+TEST_CASE(PREFIX "matchPos16", "[stringspanner]")
+{
+    const char *x = "derpderpderpderpderp";
+    std::string s(x);
+    s[16] = ',';
+    csvmonkey::StringSpanner ss(',');
+    REQUIRE(ss(s.c_str()) == 16);
+}
+
+
+TEST_CASE(PREFIX "matchPos17", "[stringspanner]")
+{
+    const char *x = "derpderpderpderpderp";
+    std::string s(x);
+    s[17] = ',';
+    csvmonkey::StringSpanner ss(',');
+    REQUIRE(ss(s.c_str()) == 16);
+}
